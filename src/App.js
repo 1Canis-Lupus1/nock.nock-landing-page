695138f1ae34f75a25c8d0a17586503850da0538
Shubham <ciruslivingstone@gmail.com>
2020-08-15 11:05:06 +0530

    Footer Layout Added with desired color

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Navbar />
       <Jumbotron />
       <Content /><br/>
-      <Testimonial />
+      <Testimonial /><br/>
       <Foot />
     </div>
   );
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Foot=()=>{
     return(
         <React.Fragment>
-            
-                Footer Contents here
-            
+            <div style={{ background: "#49BCF8", height: "calc(40vh)" }}>
+                Footer text here
+            </div>
         </React.Fragment>
     );
 }
